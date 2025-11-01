ad4d6d8906a97575f752cf26a9c5dda2765a170e
Ananya Mallekatte Basavarajappa <ananya@Ananyas-MacBook-Pro-99.local>
2025-10-31 21:12:13 -0400

    cleanup

--- line_count_mapper.py
+++
@@ -1,3 +0,0 @@
-import sys
-for line in sys.stdin:
-    print(f"{sys.argv[1] if len(sys.argv)>1 else 'unknown'}\t1")
--- line_count_reducer.py
+++
@@ -1,8 +0,0 @@
-import sys
-from collections import defaultdict
-counts = defaultdict(int)
-for line in sys.stdin:
-    file, cnt = line.strip().split('\t')
-    counts[file] += int(cnt)
-for file, total in counts.items():
-    print(f'"{file}": {total}')
